b69fb8a17e237d3bbfaf28961efdd6c65c324e18
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-11-16 16:32:04 +0100

    Added local data source, also modified with suspend some classes

--- app/src/main/java/com/antgut/rss_app/management/domain/RssRepository.kt
+++ app/src/main/java/com/antgut/rss_app/management/domain/RssRepository.kt
@@ -1,5 +1,5 @@
 package com.antgut.rss_app.management.domain
 
 interface RssRepository {
-    fun saveRss(rssModel: ManagementModel)
+    suspend fun saveRss(rssModel: ManagementModel)
 }
--- app/src/main/java/com/antgut/rss_app/management/domain/SaveRssUseCase.kt
+++ app/src/main/java/com/antgut/rss_app/management/domain/SaveRssUseCase.kt
@@ -1,7 +1,7 @@
 package com.antgut.rss_app.management.domain
 
 class SaveRssUseCase(val repository: RssRepository) {
-    fun execute(name: String, url: String) {
+    suspend fun execute(name: String, url: String) {
         repository.saveRss(ManagementModel(name, url))
     }
 }
