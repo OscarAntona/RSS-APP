4a1f9e492b0d49c528df709f9965fcc5b71589a8
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-11-29 02:06:08 +0100

    fixed conflicts merging

--- app/src/main/java/com/antgut/rss_app/MainActivity.kt
+++ app/src/main/java/com/antgut/rss_app/MainActivity.kt
@@ -1,26 +1,47 @@
-package com.antgut.rss_app
-
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.antgut.rss_app.management.data.retrofit.ApiClient
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.antgut.rss_app.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity: AppCompatActivity() {
-    val apiClient = ApiClient()
+class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupBinding()
+        setupNavigation()
+    }
 
-        GlobalScope.launch {
-            try {
-                val model = apiClient.getRss("https://e00-marca.uecdn.es/rss/baloncesto/nba.xml")
-                Log.d("@dev", "model:$model")
-            } catch (ex: java.lang.Exception) {
-                Log.d("@dev-e", ex.message!!)
-            }
+    fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.let {
+            setContentView(it.root)
         }
     }
+
+    fun setupNavigation() {
+        findViewById<BottomNavigationView>(R.id.bottom_menu).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.to_profile_item -> navigateToProfile()
+                R.id.to_manager_item -> navigateToManager()
+                R.id.to_feed_item -> navigateToFeed()
+            }
+            true
+        }
+
+    }
+
+    fun navigateToProfile() {
+        findNavController(R.id.fragment_container_view).navigate(NavGraphDirections.actionToProfile())
+    }
+
+    fun navigateToManager() {
+        findNavController(R.id.fragment_container_view).navigate(NavGraphDirections.actionToManager())
+    }
+
+    fun navigateToFeed() {
+        findNavController(R.id.fragment_container_view).navigate(NavGraphDirections.actionToFeed())
+    }
+
 }
